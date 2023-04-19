3a017154f68b7e9ff3cd144b050745f9fac5938b
Camilo Neira Gómez <camiloneira93@hotmail.com>
2023-04-19 18:13:43 -0500

    Se realizan modificaciones para la segunda entrega

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import "./App.css";
+import { Fragment, useState, useEffect } from "react";
 import NavBarComponent from "./components/NavBarComponent";
 import ItemComponentList from "./components/ItemComponentList/ItemComponentList";
+import CardComponent from "./components/Cards/CardComponent";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const BASE_URL = "https://hp-api.onrender.com/api/characters";
+  const MAX_ITEMS = 30;
+
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setItems(data.slice(0, MAX_ITEMS));
+      });
+  }, []);
+
   return (
-    <div>
-      <NavBarComponent />;
-      <ItemComponentList greeting={"Bienvenidos a Aftermath"} />;
-    </div>
+    <Fragment>
+      <NavBarComponent />
+      <ItemComponentList greeting={"Bienvenidos al mundo de Harry Potter"} />
+      <div className="container">
+        <div className="row">
+          {items.map((item, index) => (
+            <CardComponent key={item.name} data={item} />
+          ))}
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
---
+++ src/components/Cards/CardComponent.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Fragment } from "react";
+
+const CardComponent = (props) => {
+  const { index, name, gender, house, image } = props.data;
+  const { data } = props;
+
+  return (
+    <Fragment>
+      <div className="col-4">
+        <div className="card shadow-sm m-5" style={{ width: "18rem" }}>
+          <img
+            width={200}
+            height={300}
+            src={image}
+            className="card-img-top pt-3"
+            alt=""
+          />
+          <div className="card-body">
+            <h5 className="card-title">{name}</h5>
+            <p className="card-text">{house}</p>
+            <p className="card-text">{gender}</p>
+            <button className="btn btn-success btn-sm">Más detalles</button>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default CardComponent;
